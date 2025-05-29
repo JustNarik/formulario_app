be2578fa72943901bc2e05d4aa96ebb471a85ea7
JustNarik <narikdaniel2016@gmail.com>
2025-05-28 18:38:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// === Configuración de conexión a la base de datos ===
+// === Configuración de conexión a la base de datos SII ===
 $host = "tcp:erikservidor.database.windows.net,1433";
 $db = "formulario_app";
 $user = "erik";
